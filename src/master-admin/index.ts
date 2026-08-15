29e90d0a2eeeedb591da9949773d4a3af7059116
gustavogerminari-del <gustavogerminari@gmail.com>
2026-08-14 22:52:41 -0300

    fix(master): usar wrapper com menu completo no painel

--- src/master-admin/index.ts
+++ src/master-admin/index.ts
@@ -3,4 +3,6 @@ export * from './constants/planPresets';
 export * from './components/MasterTenantModal';
 export * from './components/MasterAnnouncementsModal';
 export * from './components/MasterBackupModal';
-export * from './components/MasterAdminOfficialView';
+export { MasterAdminView } from './components/MasterAdminView';
+export type { MasterNavigationSection } from './components/MasterAdminView';
+export { MasterAdminView as MasterAdminOfficialView } from './components/MasterAdminOfficialView';
